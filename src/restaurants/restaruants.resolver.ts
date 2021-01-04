c605fa9b1a52966eb5481217a6b52ce5a1d81586
happyjy <okwoyjy@gmail.com>
2021-01-04 22:31:49 +0900

    #1.2 ObjectType
    ObjectType decorator를 이용 entity 생성

--- src/restaurants/restaruants.resolver.ts
+++ src/restaurants/restaruants.resolver.ts
@@ -1,11 +1,16 @@
 import { Query, Resolver } from '@nestjs/graphql';
+import { Restaurant } from './entities/restaurant.entity';
 
 // decoration으로 이파일이 resolver 기능을 하도록 하는 기능을 한다.
-@Resolver()
+@Resolver(() => Restaurant)
 export class RestaurantResolver {
-  @Query((returns) => Boolean) // graphql을 위한 return type
-  isPizzaGood(): Boolean {
-    // typscript를 위한 return type
+  @Query((returns) => Restaurant)
+  myRestaurant() {
     return true;
   }
+  // @Query((returns) => Boolean) // graphql을 위한 return type
+  // isPizzaGood(): Boolean {
+  //   // typscript를 위한 return type
+  //   return true;
+  // }
 }
