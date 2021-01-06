b5c477e84c7aedc4f00cacc159757bc1c44fbfc7
happyjy <okwoyjy@gmail.com>
2021-01-06 23:07:21 +0900

    #2.5 Configuring ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
+import * as Joi from 'joi'; // javascript module을 import 할때는 이런 식으로 한다.
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { join } from 'path';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
@@ -10,14 +10,22 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: process.env.NODE_ENV === 'dev' ? '.dev.env' : '.test',
+      // validationSchema: Joi.object({
+      //   NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+      //   DB_HOST: Joi.string().required,
+      //   DB_PORT: Joi.string().required,
+      //   DB_USERNAME: Joi.string().required,
+      //   DB_PASSWORD: Joi.string().required,
+      //   DB_DATABASE: Joi.string().required,
+      // }),
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'jaeyoonyoon',
-      password: '12345',
-      database: 'uber-eats',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       synchronize: true,
       logging: true,
       entities: ['src/entity/**/*.ts'],
