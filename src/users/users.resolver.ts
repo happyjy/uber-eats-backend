3d9a067a0becf5f7c6dd6cb43baa3f53eba63810
happyjy <okwoyjy@gmail.com>
2021-01-10 16:16:47 +0900

    #4.9 Log In part one
    login entity, dto, resolver, service 작업

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -23,14 +23,7 @@ export class UserResolver {
     @Args('input') createAccountInput: CreateAccountInput,
   ): Promise<CreateAccountOutput> {
     try {
-      const { ok, error } = await this.usersService.createAccount(
-        createAccountInput,
-      );
-
-      return {
-        ok,
-        error,
-      };
+      return this.usersService.createAccount(createAccountInput);
     } catch (error) {
       return {
         ok: false,
@@ -40,5 +33,14 @@ export class UserResolver {
   }
 
   @Mutation((returns) => LoginOutput)
-  async login(@Args('input') loginInput: LoginInput) {}
+  async login(@Args('input') loginInput: LoginInput): Promise<LoginOutput> {
+    try {
+      return this.usersService.login(loginInput);
+    } catch (error) {
+      return {
+        ok: false,
+        error,
+      };
+    }
+  }
 }
