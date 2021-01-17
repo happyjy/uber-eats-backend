d62957a59687c25c47c58925427de3b01ad8e65f
happyjy <okwoyjy@gmail.com>
2021-01-17 18:20:43 +0900

    #6.7 Mailgun API

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,6 +1,7 @@
 import { DynamicModule, Module } from '@nestjs/common';
 import { CONFIG_OPTIONS } from 'src/common/common.constant';
-import { MailModuleOptions } from './mail.interface';
+import { MailModuleOptions } from './mail.interfaces';
+import { MailService } from './mail.service';
 
 @Module({})
 export class MailModule {
@@ -12,8 +13,9 @@ export class MailModule {
           provide: CONFIG_OPTIONS,
           useValue: options,
         },
+        MailService, // 메일 모듈에서 메일 서비스를 provide하기위해서 아래 export에 "MailService"를 설정
       ],
-      exports: [],
+      exports: [MailService],
     };
   }
 }
---
+++ src/mail/mail.service.ts
@@ -0,0 +1,39 @@
+import { Inject, Injectable } from '@nestjs/common';
+import * as FormData from 'form-data';
+import got from 'got/dist/source';
+import { CONFIG_OPTIONS } from 'src/common/common.constant';
+import { MailModuleOptions } from './mail.interfaces';
+
+@Injectable()
+export class MailService {
+  constructor(
+    @Inject(CONFIG_OPTIONS) private readonly options: MailModuleOptions,
+  ) {
+    console.log('### MailService > options: ', options);
+    this.sendEmail('testing', 'test');
+  }
+
+  private async sendEmail(subject: string, content: string) {
+    // mailgun api curl 설정
+    const form = new FormData();
+    form.append('from', `Excited User <mailgun@${this.options.domain}>`);
+    form.append('to', `okwoyjy@gmail.com`);
+    form.append('subject', subject);
+    form.append('text', content);
+
+    // mailgun으로 계속 req보내면 차단당한다는 얘기로 주석처리함.
+    // const response = await got(
+    //   `https://api.mailgun.net/v3/${this.options.domain}/messages`,
+    //   {
+    //     method: 'POST',
+    //     headers: {
+    //       Authorization: `Basic ${Buffer.from(
+    //         `api:${this.options.apiKey}`,
+    //       ).toString('base64')}`,
+    //     },
+    //     body: form,
+    //   },
+    // );
+    // console.log('### MailService: ', response.body);
+  }
+}
