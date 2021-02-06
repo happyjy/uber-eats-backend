5a105a06b7c9089920172915c663c7cec45d2108
happyjy <okwoyjy@gmail.com>
2021-02-06 09:22:02 +0900

    9.0 Setup part One

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,6 +18,8 @@ import { JwtModule } from './jwt/jwt.module';
 import { Verification } from './users/entities/verification.entity';
 // import { AuthModule } from './auth/auth.module';
 import { MailModule } from './mail/mail.module';
+
+console.log('### 210201 > process.env.NODE_ENV: ', process.env.NODE_ENV);
 @Module({
   // server 관련 설정
   imports: [
@@ -26,7 +28,7 @@ import { MailModule } from './mail/mail.module';
       envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
       ignoreEnvFile: process.env.NODE_ENV === 'prod',
       validationSchema: Joi.object({
-        NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+        NODE_ENV: Joi.string().valid('dev', 'prod', 'test').required(),
         DB_HOST: Joi.string().required(),
         DB_PORT: Joi.string().required(),
         DB_USERNAME: Joi.string().required(),
@@ -47,7 +49,8 @@ import { MailModule } from './mail/mail.module';
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       synchronize: process.env.NODE_ENV !== 'prod',
-      logging: process.env.NODE_ENV !== 'prod',
+      logging:
+        process.env.NODE_ENV !== 'prod' && process.env.NODE_ENV !== 'test',
       entities: [/* Restaurant, */ User, Verification], // table 생성 설정`
       // entities: ['src/entity/**/*.ts'],
       // migrations: ['src/migration/**/*.ts'],
---
+++ test/users.e2e-spec.ts
@@ -0,0 +1,39 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import { AppModule } from '../src/app.module';
+import { getConnection } from 'typeorm';
+
+const GRAPHQL_ENDPOINT = '/graphql';
+
+describe('UserModule (e2e)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      // AppModule을 import 해서 전체 application을 load해서, Resolver를 테스트 함
+      imports: [AppModule],
+    }).compile();
+
+    app = module.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await getConnection().dropDatabase();
+    app.close();
+  });
+
+  // it('/ (GET)', () => {
+  //   return request(app.getHttpServer())
+  //     .get('/')
+  //     .expect(200)
+  //     .expect('Hello World!');
+  // });
+
+  it.todo('can createAccount');
+  it.todo('userProfile');
+  it.todo('login');
+  it.todo('me');
+  it.todo('verifyEmail');
+  it.todo('editProfile');
+});
