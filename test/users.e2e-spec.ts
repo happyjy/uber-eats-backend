8703430cda5761f8249f2331b2754b15989f0ed1
happyjy <okwoyjy@gmail.com>
2021-02-07 14:22:26 +0900

    9.2 Testing createAccount part One

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { getConnection } from 'typeorm';
 
@@ -23,17 +24,46 @@ describe('UserModule (e2e)', () => {
     app.close();
   });
 
+  describe('can createAccount', () => {
+    const EMAIL = 'jyoon@yoon.com';
+    it('should create account', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation {
+            createAccount(input: {
+              email:"${EMAIL}",
+              password:"12345",
+              role:Owner
+            }) {
+              ok
+              error
+            }
+          }
+          `,
+        })
+        .expect(200)
+        .expect((res) => {
+          console.log('### can createAccount > res.body: ', res.body);
+
+          expect(res.body.data.createAccount.ok).toBe(true);
+          expect(res.body.data.createAccount.error).toBe(null);
+        });
+    });
+
+    it.todo('should fail if account already exists');
+  });
+  it.todo('userProfile');
+  it.todo('login');
+  it.todo('me');
+  it.todo('verifyEmail');
+  it.todo('editProfile');
+
   // it('/ (GET)', () => {
   //   return request(app.getHttpServer())
   //     .get('/')
   //     .expect(200)
   //     .expect('Hello World!');
   // });
-
-  it.todo('can createAccount');
-  it.todo('userProfile');
-  it.todo('login');
-  it.todo('me');
-  it.todo('verifyEmail');
-  it.todo('editProfile');
 });
