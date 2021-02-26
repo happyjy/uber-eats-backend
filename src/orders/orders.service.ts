70661500aca813c3e43766ab1ed77f236b045666
happyjy <okwoyjy@gmail.com>
2021-02-26 11:35:19 +0900

    11.11 Create Order part Six - culculate finish

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -25,51 +25,85 @@ export class OrderService {
     customer: User,
     { restaurantId, items }: CreateOrderInput,
   ): Promise<CreateOrderOutput> {
-    const restaurant = await this.restaurants.findOne(restaurantId);
-    if (!restaurant) {
-      return {
-        ok: false,
-        error: 'Restaurant not found',
-      };
-    }
-
-    console.log('### restaurant: ', restaurant);
-    console.log('### items: ', items);
-
-    for (const item of items) {
-      const dish = await this.dishes.findOne(item.dishId);
-      if (!dish) {
-        // abort this whole thing
+    try {
+      const restaurant = await this.restaurants.findOne(restaurantId);
+      if (!restaurant) {
         return {
           ok: false,
-          error: 'Dish not found.',
+          error: 'Restaurant not found',
         };
       }
+      console.log('### restaurant: ', restaurant);
+      console.log('### items: ', items);
 
-      console.log(`### Dish price: ${dish.price}`);
-      for (const itemOption of item.options) {
-        const dishOption = dish.options.find(
-          // dishOption: dish 테이블 option
-          // itemOption: order 주문 input data
-          (dishOption) => dishOption.name === itemOption.name,
-        );
+      let orderFinalPrice = 0;
+      const orderItems: OrderItem[] = [];
+      for (const item of items) {
+        const dish = await this.dishes.findOne(item.dishId);
+        if (!dish) {
+          // abort this whole thing
+          return {
+            ok: false,
+            error: 'Dish not found.',
+          };
+        }
 
-        if (dishOption) {
-          if (dishOption.extra) {
-            console.log(`### $USD + ${dishOption.extra}`);
-          } else {
-            const dishOptionsChoice = dishOption.choices.find(
-              (optionChoice) => optionChoice.name === itemOption.choice,
-            );
-            if (dishOptionsChoice) {
-              if (dishOptionsChoice.extra) {
-                console.log(`### $USD + ${dishOptionsChoice.extra}`);
+        console.log(`### Dish price: ${dish.price}`);
+        let dishFinalPrice = dish.price; // 메뉴에 옵션 추가 가격
+        for (const itemOption of item.options) {
+          const dishOption = dish.options.find(
+            // dishOption: dish 테이블 option
+            // itemOption: order 주문 input data
+            (dishOption) => dishOption.name === itemOption.name,
+          );
+
+          if (dishOption) {
+            if (dishOption.extra) {
+              console.log(`### $USD + ${dishOption.extra}`);
+              dishFinalPrice += dishOption.extra;
+            } else {
+              const dishOptionsChoice = dishOption.choices.find(
+                (optionChoice) => optionChoice.name === itemOption.choice,
+              );
+              if (dishOptionsChoice) {
+                if (dishOptionsChoice.extra) {
+                  console.log(`### $USD + ${dishOptionsChoice.extra}`);
+                  dishFinalPrice += dishOptionsChoice.extra;
+                }
               }
             }
           }
         }
-      }
+        orderFinalPrice += dishFinalPrice;
+
+        const orderItem = await this.orderItems.save(
+          this.orderItems.create({
+            dish,
+            options: item.options,
+          }),
+        );
+        orderItems.push(orderItem);
+      } //end - for (const itemOption of item.options)
+
+      await this.orders.save(
+        this.orders.create({
+          customer,
+          restaurant,
+          total: orderFinalPrice,
+          items: orderItems,
+        }),
+      );
+
+      return {
+        ok: true,
+      };
+    } catch (error) {
+      return {
+        ok: false,
+        error: 'Could not create order.',
+      };
     }
+
     // const order = await this.orders.save(
     //   this.orders.create({
     //     customer,
